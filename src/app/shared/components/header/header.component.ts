ba5f3b2c20b73f76ba2da3256e26cb06b85a1ab6
Caio Denzin Rosa <xsys.caio@outlook.com>
2022-11-28 09:09:30 -0300

    Criação de tela de usuário

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public change: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeMode() {
+    this.change = !this.change;
+  }
 }
